0e20d39199856516a153e73708a2d6681f5358a3
Zabi <zbvmihai@gmail.com>
2023-03-17 15:12:23 +0000

    Add register functionality with Firebase

--- app/src/main/java/com/zabava/trelloclone/activities/BaseActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/activities/BaseActivity.kt
@@ -58,8 +58,7 @@ open class BaseActivity : AppCompatActivity() {
         val snackBar = Snackbar.make(findViewById(android.R.id.content),
             message,Snackbar.LENGTH_LONG)
         val snackBarView = snackBar.view
-        snackBarView.setBackgroundColor(ContextCompat.getColor(
-            this, R.color.snackbar_error_color))
+        snackBarView.setBackgroundColor(ContextCompat.getColor(this, R.color.snackbar_error_color))
         snackBar.show()
     }
 }
--- app/src/main/java/com/zabava/trelloclone/activities/SignUpActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/activities/SignUpActivity.kt
@@ -1,13 +1,16 @@
 package com.zabava.trelloclone.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.zabava.trelloclone.R
 import com.zabava.trelloclone.databinding.ActivitySignUpBinding
 
 @Suppress("DEPRECATION")
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     private var binding: ActivitySignUpBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
@@ -19,6 +22,7 @@ class SignUpActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setupActionBar()
+        binding?.btnSignUp?.setOnClickListener { registerUser() }
     }
 
     private fun setupActionBar(){
@@ -30,5 +34,56 @@ class SignUpActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         }
         binding?.toolbarSignUpActivity?.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun registerUser(){
+        val name: String = binding?.etName?.text.toString().trim{it <= ' '}
+        val email: String = binding?.etEmail?.text.toString().trim{it <= ' '}
+        val password: String = binding?.etPassword?.text.toString().trim{it <= ' '}
+
+        if (validateForm(name,email,password)){
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                    task ->
+                hideProgressDialog()
+                if (task.isSuccessful) {
+                    val firebaseUser: FirebaseUser = task.result!!.user!!
+                    val registeredEmail = firebaseUser.email!!
+                    Toast.makeText(
+                        this,
+                        "$name successfully registered with $registeredEmail",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+                } else {
+                    Toast.makeText(
+                        this, task.exception!!.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun validateForm(name:String,email:String,password:String): Boolean{
+        return when{
+            TextUtils.isEmpty(name)->{
+                showErrorSnackBar("Please enter a name!")
+                false
+            }
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please enter an email!")
+                false
+            }
+            TextUtils.isEmpty(password)->{
+                showErrorSnackBar("Please enter a password!")
+                false
+            }
+            else->{
+                true
+            }
+
+        }
     }
 }
