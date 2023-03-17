98e420a2972143f38affbd10c9d002536d65fd3f
Zabi <zbvmihai@gmail.com>
2023-03-17 10:53:51 +0000

    Add Splash Screen
    -Splash Screen Activity added
    -Design Splash Screen
    -Add functionality for splash screen

--- app/src/main/java/com/zabava/trelloclone/MainActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.zabava.trelloclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.zabava.trelloclone.databinding.ActivityMainBinding
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        
     }
 }
