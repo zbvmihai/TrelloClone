3e370d6cf422b7ce1ab9c806bef9bde80dd453a6
Zabi <zbvmihai@gmail.com>
2023-03-17 12:01:36 +0000

    Add Sign Up Activity
    -Added Sign Up Activity
    -Added functionality  for sign up button from Intro Activity to go on sign up activity

--- app/src/main/java/com/zabava/trelloclone/IntroActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/IntroActivity.kt
@@ -1,14 +1,26 @@
 package com.zabava.trelloclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.zabava.trelloclone.databinding.ActivityIntroBinding
 
+@Suppress("DEPRECATION")
 class IntroActivity : AppCompatActivity() {
     private var binding: ActivityIntroBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityIntroBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding?.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        binding?.btnSignUpIntro?.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/zabava/trelloclone/SignUpActivity.kt
@@ -0,0 +1,33 @@
+package com.zabava.trelloclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.zabava.trelloclone.databinding.ActivitySignUpBinding
+
+@Suppress("DEPRECATION")
+class SignUpActivity : AppCompatActivity() {
+    private var binding: ActivitySignUpBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding?.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding?.toolbarSignUpActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        }
+        binding?.toolbarSignUpActivity?.setNavigationOnClickListener { onBackPressed() }
+    }
+}
