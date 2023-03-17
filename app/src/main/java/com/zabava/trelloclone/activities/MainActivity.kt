772cc99eddd30f4d950a2932bb5cc343a12a34c0
Zabi <zbvmihai@gmail.com>
2023-03-17 19:19:01 +0000

    Add side menu to main activity
    -Added Hamburger button in navbar
    -Added a menu view when hamburger button clicked
    -Added menu view functionality to populate with user data from firebase
    -Added my profile and sign out button
    -Added sign out button functionality

--- app/src/main/java/com/zabava/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/activities/MainActivity.kt
@@ -2,13 +2,21 @@ package com.zabava.trelloclone.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.zabava.trelloclone.R
 import com.zabava.trelloclone.databinding.ActivityMainBinding
+import com.zabava.trelloclone.firebase.FirestoreClass
+import com.zabava.trelloclone.models.User
 
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +24,66 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        setupActionBar()
+
+        binding?.navView?.setNavigationItemSelectedListener(this)
+
+        FirestoreClass().signInUser(this)
+
+    }
+
+    fun updateNavigationUserDetails(user: User){
+
+        val ivUserProfile = findViewById<ImageView>(R.id.iv_user_image)
+        Glide
+            .with(this)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_user_place_holder)
+            .into(ivUserProfile)
+
+        val tvUserName = findViewById<TextView>(R.id.tv_username)
+        tvUserName.text = user.name
+
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding?.includeBar?.toolbarMainActivity)
+        binding?.includeBar?.toolbarMainActivity?.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+        binding?.includeBar?.toolbarMainActivity?.setNavigationOnClickListener {
+
+            if (binding?.drawerLayout?.isDrawerOpen(GravityCompat.START)!!){
+                binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+            }else{
+                binding?.drawerLayout?.openDrawer(GravityCompat.START)
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (binding?.drawerLayout?.isDrawerOpen(GravityCompat.START)!!){
+            binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile -> {
+                Toast.makeText(this,"My Profile Clicked",Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
+        binding?.drawerLayout?.closeDrawer(GravityCompat.START)
+
+        return true
     }
 }
--- app/src/main/java/com/zabava/trelloclone/firebase/FirestoreClass.kt
+++ app/src/main/java/com/zabava/trelloclone/firebase/FirestoreClass.kt
@@ -1,8 +1,12 @@
 package com.zabava.trelloclone.firebase
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.zabava.trelloclone.activities.MainActivity
 import com.zabava.trelloclone.activities.SignInActivity
 import com.zabava.trelloclone.activities.SignUpActivity
 import com.zabava.trelloclone.models.User
@@ -13,29 +17,49 @@ class FirestoreClass {
 
     private val mFireStore = FirebaseFirestore.getInstance()
 
-    fun registerUser(activity: SignUpActivity, userInfo: User){
-        mFireStore.collection(Constants.USERS).document(getCurrentUserId()).set(userInfo,
-            SetOptions.merge()).addOnSuccessListener {
-                activity.userRegisteredSuccess()
+    fun registerUser(activity: SignUpActivity, userInfo: User) {
+        mFireStore.collection(Constants.USERS).document(getCurrentUserId()).set(
+            userInfo,
+            SetOptions.merge()
+        ).addOnSuccessListener {
+            activity.userRegisteredSuccess()
         }
     }
 
-    fun signInUser(activity: SignInActivity){
+    @SuppressLint("SuspiciousIndentation")
+    fun signInUser(activity: Activity) {
         mFireStore.collection(Constants.USERS)
             .document(getCurrentUserId())
             .get()
             .addOnSuccessListener { document ->
-            val loggedInUser = document.toObject(User::class.java)
+                val loggedInUser = document.toObject(User::class.java)
 
-                activity.signInSuccess(loggedInUser!!)
-        }
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.signInSuccess(loggedInUser!!)
+                    }
+                    is MainActivity -> {
+                        activity.updateNavigationUserDetails(loggedInUser!!)
+                    }
+                }
+            }.addOnFailureListener { e ->
+                when (activity) {
+                    is SignInActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                    is MainActivity -> {
+                        activity.hideProgressDialog()
+                    }
+                }
+                Log.e("SignInUser", "Error writing document", e)
+            }
     }
 
-    fun getCurrentUserId():String{
+    fun getCurrentUserId(): String {
 
-        var currentUser = FirebaseAuth.getInstance().currentUser
+        val currentUser = FirebaseAuth.getInstance().currentUser
         var currentUserID = ""
-        if (currentUser!= null){
+        if (currentUser != null) {
             currentUserID = currentUser.uid
         }
         return currentUserID
