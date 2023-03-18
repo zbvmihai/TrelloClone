92688695ca54235381f81a5e45a3194bcc6cbffa
Zabi <zbvmihai@gmail.com>
2023-03-18 17:37:25 +0000

    Add functionality for Board Image View
    -Added functionality for board image view to open the gallery and select a picture and fill the image view with the selected image.

--- app/src/main/java/com/zabava/trelloclone/activities/CreateBoardActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/activities/CreateBoardActivity.kt
@@ -1,12 +1,25 @@
 package com.zabava.trelloclone.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.zabava.trelloclone.R
 import com.zabava.trelloclone.databinding.ActivityCreateBoardBinding
+import com.zabava.trelloclone.utils.Constants
+import java.io.IOException
 
 @Suppress("DEPRECATION")
 class CreateBoardActivity : AppCompatActivity() {
+
+    private var mSelectedImageFileURI: Uri? = null
 
     private var binding: ActivityCreateBoardBinding? = null
 
@@ -19,7 +32,23 @@ class CreateBoardActivity : AppCompatActivity() {
         setupActionBar()
 
         binding?.toolbarCreateBoardActivity?.setNavigationOnClickListener { onBackPressed() }
+        binding?.ivBoardImage?.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
 
+                Constants.showImageChooser(this)
+
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Constants.READ_STORAGE_PERMISSION_CODE
+                )
+            }
+        }
     }
 
     private fun setupActionBar() {
@@ -29,5 +58,45 @@ class CreateBoardActivity : AppCompatActivity() {
         actionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         actionBar?.title = resources.getString(R.string.create_board_title)
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                Constants.showImageChooser(this)
+            } else {
+                Toast.makeText(this, "Permission for storage denied.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK
+            && requestCode == Constants.PICK_IMAGE_REQUEST_CODE
+            && data!!.data != null
+        ) {
+            mSelectedImageFileURI = data.data
+
+            try {
+                Glide
+                    .with(this)
+                    .load(mSelectedImageFileURI)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_board_place_holder)
+                    .into(binding?.ivBoardImage!!)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
