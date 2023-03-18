e2630e7d206b61d6c46f5631e24414eed619e58d
Zabi <zbvmihai@gmail.com>
2023-03-18 13:21:46 +0000

    Add functionality to update the profile
    -Added functionality to update the profile details with the edited username, mobile and selected profile picture from the gallery. 
    -When update button is clicked, the details are updated in firebase data base.
    -The selected image is now uploaded in firebase storage and the link in the database is update accordingly

--- app/src/main/java/com/zabava/trelloclone/activities/MyProfileActivity.kt
+++ app/src/main/java/com/zabava/trelloclone/activities/MyProfileActivity.kt
@@ -8,25 +8,32 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.zabava.trelloclone.R
 import com.zabava.trelloclone.databinding.ActivityMyProfileBinding
 import com.zabava.trelloclone.firebase.FirestoreClass
 import com.zabava.trelloclone.models.User
+import com.zabava.trelloclone.utils.Constants
 import java.io.IOException
 
 @Suppress("DEPRECATION")
 class MyProfileActivity : BaseActivity() {
 
-    companion object{
+    companion object {
         private const val READ_STORAGE_PERMISSION_CODE = 1
         private const val PICK_IMAGE_REQUEST_CODE = 2
     }
 
     private var mSelectedImageFileURI: Uri? = null
+    private var mProfileImageURL: String = ""
+    private lateinit var mUserDetails: User
 
     private var binding: ActivityMyProfileBinding? = null
 
@@ -42,22 +49,32 @@ class MyProfileActivity : BaseActivity() {
 
         FirestoreClass().loadUserData(this)
 
-        binding?.ivUserProfileImage?.setOnClickListener{
+        binding?.ivUserProfileImage?.setOnClickListener {
 
             if (ContextCompat.checkSelfPermission(
-                    this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                        == PackageManager.PERMISSION_GRANTED){
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                == PackageManager.PERMISSION_GRANTED
+            ) {
 
                 showImageChooser()
 
-            }else{
+            } else {
                 ActivityCompat.requestPermissions(
                     this,
                     arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
                     READ_STORAGE_PERMISSION_CODE
                 )
             }
+        }
 
+        binding?.btnUpdate?.setOnClickListener {
+            if(mSelectedImageFileURI != null){
+                uploadUserImage()
+            }else{
+                showProgressDialog(resources.getString(R.string.please_wait))
+                updateUserProfileData()
+            }
         }
     }
 
@@ -67,19 +84,22 @@ class MyProfileActivity : BaseActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == READ_STORAGE_PERMISSION_CODE){
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
             if (grantResults.isNotEmpty()
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
                 showImageChooser()
-            }else{
-                Toast.makeText(this,"Permission for storage denied.",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission for storage denied.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun showImageChooser(){
-        val galleryIntent = Intent(Intent.ACTION_PICK,
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    private fun showImageChooser() {
+        val galleryIntent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
         startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
     }
 
@@ -89,7 +109,8 @@ class MyProfileActivity : BaseActivity() {
 
         if (resultCode == Activity.RESULT_OK
             && requestCode == PICK_IMAGE_REQUEST_CODE
-            && data!!.data != null){
+            && data!!.data != null
+        ) {
             mSelectedImageFileURI = data.data
 
             try {
@@ -99,13 +120,13 @@ class MyProfileActivity : BaseActivity() {
                     .centerCrop()
                     .placeholder(R.drawable.ic_user_place_holder)
                     .into(binding?.ivUserProfileImage!!)
-            }catch (e: IOException){
+            } catch (e: IOException) {
                 e.printStackTrace()
             }
         }
     }
 
-    private fun setupActionBar(){
+    private fun setupActionBar() {
         setSupportActionBar(binding?.toolbarMyProfileActivity)
 
         val actionBar = supportActionBar
@@ -115,7 +136,10 @@ class MyProfileActivity : BaseActivity() {
 
     }
 
-    fun setUserDataInUI(user: User){
+    fun setUserDataInUI(user: User) {
+
+        mUserDetails = user
+
         Glide
             .with(this)
             .load(user.image)
@@ -125,8 +149,80 @@ class MyProfileActivity : BaseActivity() {
 
         binding?.etName?.setText(user.name)
         binding?.etEmail?.setText(user.email)
-        if (user.mobile != 0L){
+        if (user.mobile != 0L) {
             binding?.etMobile?.setText(user.mobile.toString())
         }
+    }
+
+    private fun updateUserProfileData(){
+        val userHashMap = HashMap<String, Any>()
+
+        var anyChangesMade = false
+
+        if (mProfileImageURL.isNotEmpty()
+            && mProfileImageURL != mUserDetails.image){
+            userHashMap[Constants.IMAGE] = mProfileImageURL
+            anyChangesMade = true
+        }
+
+        if (binding?.etName?.text.toString() != mUserDetails.name){
+            userHashMap[Constants.NAME] = binding?.etName?.text.toString()
+            anyChangesMade = true
+        }
+        if (binding?.etMobile?.text.toString() != mUserDetails.mobile.toString()){
+            userHashMap[Constants.MOBILE] = binding?.etMobile?.text.toString().toLong()
+            anyChangesMade = true
+        }
+        if (anyChangesMade){
+            FirestoreClass().updateUserProfileData(this,userHashMap)
+        }
+    }
+
+    private fun uploadUserImage(){
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        if (mSelectedImageFileURI !=  null){
+
+            val sRef : StorageReference =
+                FirebaseStorage.getInstance().reference.child("USER_IMAGE"
+                        + System.currentTimeMillis()+ "."
+                        + getFileExtension(mSelectedImageFileURI!!))
+
+            sRef.putFile(mSelectedImageFileURI!!).addOnSuccessListener {
+                taskSnapshot ->
+                Log.i(
+                    "Firebase Image URL", taskSnapshot.metadata!!.reference!!.downloadUrl.toString()
+                )
+
+                taskSnapshot.metadata!!.reference!!.downloadUrl.addOnSuccessListener {
+                    uri ->
+                    Log.i(
+                        "Downloadable Image URL", uri.toString()
+                    )
+                    mProfileImageURL = uri.toString()
+
+                    updateUserProfileData()
+                }
+
+            }.addOnFailureListener{
+                exception ->
+                Toast.makeText(this@MyProfileActivity,
+                    exception.message,
+                    Toast.LENGTH_SHORT)
+                    .show()
+
+                hideProgressDialog()
+            }
+        }
+    }
+
+    private fun getFileExtension(uri: Uri): String? {
+        return MimeTypeMap.getSingleton().
+        getExtensionFromMimeType(contentResolver.getType(uri))
+    }
+
+    fun profileUpdateSuccess(){
+        hideProgressDialog()
+        finish()
     }
 }
--- app/src/main/java/com/zabava/trelloclone/firebase/FirestoreClass.kt
+++ app/src/main/java/com/zabava/trelloclone/firebase/FirestoreClass.kt
@@ -3,6 +3,7 @@ package com.zabava.trelloclone.firebase
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
@@ -24,6 +25,24 @@ class FirestoreClass {
         ).addOnSuccessListener {
             activity.userRegisteredSuccess()
         }
+    }
+
+    fun updateUserProfileData(activity: MyProfileActivity,
+                              userHashMap: HashMap<String,Any>){
+
+        mFireStore.collection(Constants.USERS)
+            .document(getCurrentUserId())
+            .update(userHashMap)
+            .addOnSuccessListener {
+                Log.e(activity.javaClass.simpleName,"Profile Data updated successfully!")
+                Toast.makeText(activity,"Profile Updated Successfully!",Toast.LENGTH_SHORT).show()
+                activity.profileUpdateSuccess()
+            }.addOnFailureListener {
+                e ->
+                activity.hideProgressDialog()
+                Log.e(activity.javaClass.simpleName,"Error while creating a board.",e)
+                Toast.makeText(activity,"Profile Updated Error!",Toast.LENGTH_SHORT).show()
+            }
     }
 
     @SuppressLint("SuspiciousIndentation")
